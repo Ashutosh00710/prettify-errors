39286c6a2da62e1a9fe6874ef3573cea6cd06a61
Ashutosh <ashutosh.dwivedi@kampd.com>
2022-11-16 12:29:12 +0530

    update: documantation

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'pretty-errors'
+const libraryName = 'prettify-errors'
 
 export default {
   input: `src/${libraryName}.ts`,
